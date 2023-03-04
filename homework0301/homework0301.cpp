f33fcec06faecbe3c5bc065c4e1a3de710982539
ilshat <ilshat@ilshat.com>
2023-03-04 08:42:22 +0500

    homework 03* add

---
+++ homework0301/homework0301.cpp
@@ -0,0 +1,94 @@
+﻿#include <iostream>
+class Calculator
+{
+public:
+    double num1;
+    double num2;
+    double add();
+    double multiply();
+    double subtract_1_2();
+    double subtract_2_1();
+    double divide_1_2();
+    double divide_2_1();
+    bool set_num1(double num1);
+    bool set_num2(double num2);
+};
+double Calculator::add()
+{
+    return num1 + num2;
+}
+double Calculator::multiply()
+{
+    return num1 * num2;
+}
+double Calculator::subtract_1_2()
+{
+    return num1 - num2;
+}
+double Calculator::subtract_2_1()
+{
+    return num2 - num1;
+}
+double Calculator::divide_1_2()
+{
+    return num1 / num2;
+}
+double Calculator::divide_2_1()
+{
+    return num2 / num1;
+}
+
+bool Calculator::set_num1(double num1)
+{
+    if (num1 != 0) {
+        this->num1 = num1;
+        return true;
+    }
+    
+    return false;
+}
+
+bool Calculator::set_num2(double num2)
+{
+    if (num2 != 0) {
+        this->num2 = num2;
+        return true;
+    }
+
+    return false;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    while (true)
+    {
+        std::cout << "Введите num1: ";
+        double num1;
+        std::cin >> num1;
+
+        std::cout << "Введите num2: ";
+        double num2;
+        std::cin >> num2;
+
+        Calculator calc{ num1,num2 };
+
+        if (calc.set_num1(num1) && calc.set_num2(num2))
+        {
+            std::cout << "num1 + num2 = " << calc.add() << std::endl;
+            std::cout << "num1 * num2 = " << calc.multiply() << std::endl;
+            std::cout << "num1 - num2 = " << calc.subtract_1_2() << std::endl;
+            std::cout << "num2 - num1 = " << calc.subtract_2_1() << std::endl;
+            std::cout << "num1 / num2 = " << calc.divide_1_2() << std::endl;
+            std::cout << "num2 / num1 = " << calc.divide_2_1() << std::endl;
+        }
+        else
+        {
+            std::cout << "Неверный ввод!" << std::endl;
+        }
+
+
+    }
+
+}
---
+++ homework0302/homework0302.cpp
@@ -0,0 +1,95 @@
+﻿#include <iostream>
+class Counter
+{
+public:
+	Counter();
+	Counter(int num);
+	void num_inc() { num++; }
+	void num_dec() { num--; }
+	int num_disp() { return num; }
+private:
+	int num;
+};
+Counter::Counter(int num)
+{
+	this->num = num;
+}
+Counter::Counter()
+{
+	this->num = 1;
+}
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+		std::cout << "Вы хотите указать начальное значение счётчика? Введите yes/no: ";
+		std::string yes_or_no;
+		std::cin >> yes_or_no;
+		if (yes_or_no == "yes")
+		{
+			std::cout << "Введите начальное значение счётчика: ";
+			int n;
+			std::cin >> n;
+			Counter count1(n);
+
+			bool loop = true;
+			while (loop)
+			{
+				std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+				char command;
+				std::cin >> command;
+				switch (command)
+				{
+				case '+':
+					count1.num_inc();
+					break;
+				case '-':
+					count1.num_dec();
+					break;
+				case '=':
+					std::cout << count1.num_disp() << std::endl;
+					break;
+				case 'x':
+					std::cout << "До свидания!\n";
+					loop = false;
+					break;
+				default:
+					break;
+				}
+			}
+		}
+		else if (yes_or_no == "no")
+		{
+			Counter count1;
+
+			bool loop = true;
+			while (loop)
+			{
+				std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+				char command;
+				std::cin >> command;
+				switch (command)
+				{
+				case '+':
+					count1.num_inc();
+					break;
+				case '-':
+					count1.num_dec();
+					break;
+				case '=':
+					std::cout << count1.num_disp() << std::endl;
+					break;
+				case 'x':
+					std::cout << "До свидания!\n";
+					loop = false;
+					break;
+				default:
+					break;
+				}
+			}
+		}
+
+
+}
+
+
