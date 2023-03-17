d302b0db7e8b0a5640975df50bd1a978d1357505
ilshat <ilshat@ilshat.com>
2023-03-17 12:40:34 +0500

    homework done

---
+++ homework0701/homework0701.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#define MODE 1
+
+int add(int num1, int num2) {
+    return num1 + num2;
+}
+
+int main()
+{
+    setlocale(0, "ru");
+
+#if MODE == 0
+    std::cout << "Работаю в режиме тренировки\n";
+#elif MODE == 1
+    std::cout << "Работаю в боевом режиме\n";
+
+    std::cout << "Введите число 1: ";
+    int num1;
+    std::cin >> num1;
+    std::cout << "Введите число 2: ";
+    int num2;
+    std::cin >> num2;
+    std::cout << "Результат сложения: " << add(num1, num2);
+
+#else
+#error Неизвестный режим. Завершение работы
+#endif // MODE == 0
+
+
+
+
+}
